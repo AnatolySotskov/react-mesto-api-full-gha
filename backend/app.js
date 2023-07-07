ed57a32cd5188e790e08d544536a7160fc2b5a23
Anatoly Sotskov <anatolysotskov@ynande.ru>
2023-07-07 23:50:48 +0300

    last last

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const router = require('./routes');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
