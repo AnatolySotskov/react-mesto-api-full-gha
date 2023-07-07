5fdae43dc738b5b34e12bda6906ef8b72eef3d8e
Anatoly Sotskov <anatolysotskov@ynande.ru>
2023-07-07 22:41:46 +0300

    aaaaaa2

--- backend/controllers/card.js
+++ backend/controllers/card.js
@@ -35,11 +35,9 @@ const deleteCard = (req, res, next) => {
       if (dataCard.owner.toString() !== userId) {
         throw new Forbidden('Нельзя удалить чужую карточку (Ошибка 403)');
       }
-      const cardDelete = card.findByIdAndRemove(cardId);
-      res.status(VERY_GOOD).send(cardDelete);
-      // return card
-      //   .findByIdAndRemove(cardId)
-      //   .then(() => res.status(VERY_GOOD).send(card));
+      return card
+        .findByIdAndRemove(cardId)
+        .then(() => res.status(VERY_GOOD).send(card));
     })
     .catch((err) => {
       next(err);
