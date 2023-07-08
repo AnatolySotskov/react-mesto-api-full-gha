1e979b65b407bfec0293ec0ef37c383d3d7bd80a
Anatoly Sotskov <anatolysotskov@ynande.ru>
2023-07-08 10:58:47 +0300

    zlo2

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,9 @@ const router = require('./routes');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors());
-
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
